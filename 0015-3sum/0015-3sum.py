32988fde126476e4f8bbb4064619c35d86b89da0
Akshanth Mamidala <119917995+itsthekidash3@users.noreply.github.com>
2026-01-26 08:40:39 -0700

    Time: 562 ms (68.17%), Space: 22.5 MB (13.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,7 +5,8 @@ class Solution:
 
         for tar,val in enumerate(nums):
             if  tar > 0 and val == nums[tar-1]: 
-                continue # this is not the first value in input array and it is equal to the previous number
+                continue # this is not the first value in input array and it is equal to the previous number 
+                         #we don’t start a new triplet with the same a twice.
             left,right = tar+1, len(nums) - 1 # two pointers
             while left < right:
                 target = val + nums[left] + nums[right]
@@ -18,5 +19,6 @@ class Solution:
                     left += 1 #update any one pointer after appending to arr
                             #the conditions will handle the other pointer
                     while nums[left] == nums[left-1] and left<right: #we dont want the same value
+                                                                # This check removes duplicate second elements in inner loop
                         left += 1
         return arr
