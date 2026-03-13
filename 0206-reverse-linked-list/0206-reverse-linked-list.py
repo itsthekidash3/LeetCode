4da48e0cf8695197692778869328f7896985878f
Akshanth Mamidala <119917995+itsthekidash3@users.noreply.github.com>
2026-03-12 23:37:46 -0600

    Time: 0 ms (100%), Space: 20.6 MB (59.98%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,13 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
+        prev = None # prev pointer is set to null
+        curr = head # initalize current to the first node to set it a the last
         while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
+            # at the curr node
+            nxt = curr.next # temp variable
+            curr.next = prev # pointing to prev
+            # need to shift the node after iteration
+            prev = curr # shift it to current 
+            # curr = curr.next
+            # need a temp variable to store the next , coz we are breaking the pointer
+            curr = nxt # shift it to the next
         
         return prev
 
